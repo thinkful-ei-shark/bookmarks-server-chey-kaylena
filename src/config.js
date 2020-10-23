a1a496634872b2d6bf26e4db7170c3d0aede0a8b
Chey Rose Flammer <cheyroseflammer@gmail.com>
2020-10-23 09:39:42 -0700

    added logger and api token key/module

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || "development",
+  API_TOKEN: process.env.API_TOKEN || '9169cea4-4eeb-4de5-b001-1ed5b6fae455'
 };
